312313889004652f87dc398e6f9679aac27047e8
João Alisson <76262867+JoaoAlissonTec@users.noreply.github.com>
2024-01-07 08:25:47 -0300

    Add Pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Container from './components/layouts/Container';
+
 function App() {
   return (
-    <div className="App">
-      <p>Cost</p>
-    </div>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </ul>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/company' element={<Company/>}/>
+          <Route exact path='/contact' element={<Contact/>}/>
+          <Route exact path='/newproject' element={<NewProject/>}/>
+        </Routes>
+      </Container>
+      <footer>Footer</footer>
+    </Router>
   );
 }
 
