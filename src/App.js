2c23531007a947ecc79b0b74f3ca76db89caeb4a
João Alisson <76262867+JoaoAlissonTec@users.noreply.github.com>
2024-01-07 08:52:56 -0300

    Add navbar/footer

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+
 import Container from './components/layouts/Container';
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
+import Projects from './components/pages/Projects';
 
 function App() {
   return (
     <Router>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </ul>
+      <Navbar/>
       <Container customClass="min-height">
         <Routes>
           <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/projects' element={<Projects/>}/>
           <Route exact path='/company' element={<Company/>}/>
           <Route exact path='/contact' element={<Contact/>}/>
           <Route exact path='/newproject' element={<NewProject/>}/>
         </Routes>
       </Container>
-      <footer>Footer</footer>
+      <Footer/>
     </Router>
   );
 }
