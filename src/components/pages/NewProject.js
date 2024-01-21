43f4bf7a568813ce942606bd3d59b1b4e2ffe271
João Alisson <76262867+JoaoAlissonTec@users.noreply.github.com>
2024-01-21 07:29:54 -0300

    Add Message

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,55 @@
+import {useNavigate} from 'react-router-dom'
 import ProjectForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
 
 function NewProject(){
+
+    const navigate = useNavigate()
+
+    function stringify(obj) {
+        let cache = [];
+        let str = JSON.stringify(obj, function(key, value) {
+          if (typeof value === "object" && value !== null) {
+            if (cache.indexOf(value) !== -1) {
+              // Circular reference found, discard key
+              return;
+            }
+            // Store value in our collection
+            cache.push(value);
+          }
+          return value;
+        });
+        cache = null; // reset the cache
+        return str;
+      }
+
+    function createPost(project){
+        //initialize cost and services
+        project.cost = 0
+        project.service = []
+
+        fetch("http://localhost:5000/projects", {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json',
+            },
+            body: stringify(project),
+        })
+        .then((resp)=>resp.json)
+        .then((data)=>{
+            console.log(data)
+            //redirect
+            const state = {message: "Projeto criado com sucesso!"}
+            navigate("/projects", {state})
+        })
+        .catch((err)=>console.log(err))
+    }
+
     return (
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm btnText="Criar Projeto"/>
+            <ProjectForm handleSubmit={createPost} btnText="Criar Projeto"/>
         </div>
     )
 }
